03d1ef643e69ec716237dc9f7d1313df98e33007
Adrian <kusek.adrian@gmail.com>
2024-09-05 14:28:40 +0200

    created react app added pallete

---
+++ src/Pallete.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Pallete() {
+  return (
+    <div className='Pallete'>
+        {/* navbar goes here */}
+        <div className='Pallete-colors'>
+            {/* bunch of color boxes */}
+        </div>
+        {/* footer later */}
+    </div>
+  )
+}
