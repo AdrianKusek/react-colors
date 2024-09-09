b4aa7c502698a0775ac99bf16bec540c785ec6b9
Adrian <kusek.adrian@gmail.com>
2024-09-09 12:30:56 +0200

    add ColorHelper to generete colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Pallete from "./Pallete";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
+  console.log(generatePalette(seedColors[4]))
   return (
     <div>
       <Pallete {...seedColors[4]}/>
