59f2d2fb8990b3d0ef95ba888d6c401737b9afc5
Adrian <kusek.adrian@gmail.com>
2024-09-10 14:21:44 +0200

    finish styling mini palette

--- src/MiniPallete.js
+++ src/MiniPallete.js
@@ -3,11 +3,13 @@ import { Box, Typography } from '@mui/material';
 import MiniPaletteStyles from './styles/MiniPaletteStyles';
 
 export default function MiniPalette(props) {
-  const {paletteName, emoji}  = props
-  return (
+  const {paletteName, emoji,colors}  = props
+  const miniColorBoxes = colors.map((color) => (
+    <Box sx={{ ...MiniPaletteStyles.miniColor, backgroundColor: color.color }} key={color.name}></Box>
+  ));  return (
     <Box sx={MiniPaletteStyles.root}>
       <Box sx={MiniPaletteStyles.colors}>
-        {/* Your color boxes or other content here */}
+        {miniColorBoxes}
       </Box>
       <Typography variant="h5" sx={MiniPaletteStyles.title}>
         {paletteName}
