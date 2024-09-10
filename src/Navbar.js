b130cad694546235bb71cb45f619baff5c57a214
Adrian <kusek.adrian@gmail.com>
2024-09-10 07:09:51 +0200

    add select to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './Navbar.css'
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function Navbar(props) {
   const {level,changeLevel}  = props
+  const [format, setFormat] = useState('hex')
+  const handleChange = (e) =>{
+    setFormat(e.target.value)
+    props.handleChange(e.target.value)
+
+  }
   return (
    <header className='Navbar'>
     <div className='logo'>
@@ -18,6 +26,14 @@ export default function Navbar(props) {
 
         </div>
 
+        </div>
+        <div className='select-container'>
+            <Select value={format} onChange={handleChange}>
+                <MenuItem value='hex' >Hex  </MenuItem>
+                <MenuItem value='rgb' >rgb </MenuItem>
+                <MenuItem value='rgba' >rgba </MenuItem>
+
+            </Select>
         </div>
         
    
--- src/Pallete.js
+++ src/Pallete.js
@@ -6,15 +6,21 @@ import Navbar from './Navbar';
 
 export default function Pallete(props) {
   const [level, setLevel]  = useState(500)  
-  const colorBoxes = props.pallete.colors[level].map(color =>(<ColorBox background={color.hex} name={color.name}/>))
+  const [format, setFormat] = useState('hex')
+ 
+  const colorBoxes = props.pallete.colors[level].map(color =>(<ColorBox background={color[format]} name={color.name}/>))
   const changeLevel = (newLevel)=>{
     setLevel(newLevel)
 
-  }  
+  } 
+  const changeFormat = (val) =>{
+   setFormat(val)
+
+  }
   return (
     <div className='Pallete'>
 
-        <Navbar level = {level} changeLevel = {changeLevel}/>
+        <Navbar level = {level} changeLevel = {changeLevel} handleChange={changeFormat}/>
        
         {/* navbar goes here */}
         <div className='Pallete-colors'>
